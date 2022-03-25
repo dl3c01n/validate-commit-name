6d21d6e81b19761c331c9656fe99a04bbaae36bc
unknown <dl3c01n>
2022-03-25 14:56:26 +0100

    clog

--- index.js
+++ index.js
@@ -6,17 +6,21 @@ try {
   const commitName = core.getInput("commit-name");
 
   if(commitName.toLowerCase().startsWith('fix(')){
+    
     const numbers = commitName.substring(4)
     const commitNumber = numbers.substring(0, numbers.indexOf(')'))
-    core.setOutput('response', `Your commit number is ${commitNumber}`)
+    core.setOutput('response', `Your commit number is ${commitNumber}`)*
+    core.info(`Your commit number is ${commitNumber}`)
   }else if(commitName.toLowerCase().startsWith('feature(')){
     const numbers = commitName.substring(8)
     const commitNumber = numbers.substring(0, numbers.indexOf(')'))
     core.setOutput('response', `Your commit number is ${commitNumber}`)
+    core.info(`Your commit number is ${commitNumber}`)
   }else if(commitName.toLowerCase().startsWith('hotfix(')){
     const numbers = commitName.substring(7)
     const commitNumber = numbers.substring(0, numbers.indexOf(')'))
     core.setOutput('response', `Your commit number is ${commitNumber}`)
+    core.info(`Your commit number is ${commitNumber}`)
   }else {
     core.setFailed("Commit must start with fix/feature/hotfix");
   }
