b0e685a8084f8692694a25d5269ae4df0d8b3858
unknown <dl3c01n>
2022-03-25 14:49:27 +0100

    changed maner

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ try {
   // `who-to-greet` input defined in action metadata file
   const commitName = core.getInput("commit-name");
 
-  if(!commitName.toString().toLowerCase().startsWith('fix(') || !commitName.toString().toLowerCase().startsWith('feature(') ||!commitName.toString().toLowerCase().startsWith('hotfix(')){
-    core.setFailed("Commit must start with fix/feature/hotfix");
-  }
-
   if(commitName.toLowerCase().startsWith('fix(')){
     const numbers = commitName.substring(4)
     const commitNumber = numbers.substring(0, numbers.indexOf(')'))
@@ -21,6 +17,8 @@ try {
     const numbers = commitName.substring(7)
     const commitNumber = numbers.substring(0, numbers.indexOf(')'))
     core.setOutput('response', `Your commit number is ${commitNumber}`)
+  }else {
+    core.setFailed("Commit must start with fix/feature/hotfix");
   }
 } catch (error) {
   core.setFailed(error.message);
