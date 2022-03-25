6a9e8ce9ad8640a94abca9768ddacdc63f52d654
unknown <dl3c01n>
2022-03-25 14:40:33 +0100

    numberss

--- index.js
+++ index.js
@@ -5,8 +5,22 @@ try {
   // `who-to-greet` input defined in action metadata file
   const commitName = core.getInput("commit-name");
 
-  if(!commitName.toLowerCase().startsWith('fix') || !commitName.toLowerCase().startsWith('feature') ||!commitName.toLowerCase().startsWith('hotfix')){
+  if(!commitName.toLowerCase().startsWith('fix(') || !commitName.toLowerCase().startsWith('feature(') ||!commitName.toLowerCase().startsWith('hotfix(')){
     core.setFailed("Commit must start with fix/feature/hotfix");
+  }
+
+  if(commitName.toLowerCase().startsWith('fix(')){
+    const numbers = commitName.substring(4)
+    const commitNumber = numbers.substring(0, numbers.indexOf(')'))
+    core.setOutput('response', `Your commit number is ${commitNumber}`)
+  }else if(commitName.toLowerCase().startsWith('feature(')){
+    const numbers = commitName.substring(8)
+    const commitNumber = numbers.substring(0, numbers.indexOf(')'))
+    core.setOutput('response', `Your commit number is ${commitNumber}`)
+  }else if(commitName.toLowerCase().startsWith('hotfix(')){
+    const numbers = commitName.substring(7)
+    const commitNumber = numbers.substring(0, numbers.indexOf(')'))
+    core.setOutput('response', `Your commit number is ${commitNumber}`)
   }
 } catch (error) {
   core.setFailed(error.message);
